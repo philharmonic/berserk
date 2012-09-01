f5f50b62ff1a0267c389fb8f136ae52cb7a197c8
Dražen Lučanin <kermit666@gmail.com>
2012-09-01 21:28:27 +0200

    The memory C extension.

--- berserk.py
+++ berserk.py
@@ -1,15 +1,41 @@
-class Structure(object):
-    data = "abcd"
-    def __init__(self, l):
-        self.reserved = l*self.data
+def log(something):
+    print(something)
 
-def memory():
-    s = Structure(10000)
+
+# MEMORY
+#---------------------
+# import sys
+
+# class Structure(object):
+#     data = "abcd"
+#     def __init__(self, l):
+#         self.reserved = l*self.data
+
+# def memory(size = 1):
+#     """
+#     @param size: MBs to allocate
+#     """
+#     # allocate
+#     data = range(10000)
+#     size = sys.getsizeof(data)
+#     log("allocated %d bytes" % (size))
+#     # read / write
+#---------------------
+
 def cpu():
     a = 0.8*2.3
 
-if __name__ == "__main__":
+def run_from_conf(conf):
+    #memory(size=conf.size)
+    import memory
+    memory.run(10**3)
+
+def sample_run():
     while True:
         memory()
         cpu()
-    
+
+if __name__ == "__main__":
+    #sample_run()
+    import conf
+    run_from_conf(conf)
